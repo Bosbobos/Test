661e0491bde8d819077bca5560b8b70f9d3dffb3
Bosbobos <konovalov.matvej@mail.ru>
2020-03-03 18:21:14 +0300

    Глава вторая: нормальный коммитер

--- test.js
+++ test.js
@@ -1,5 +1,11 @@
+// Первый квест
+
 //console.log("Hello World!")
 var i
-for(i = 0; i <= 150; i++){
+for(i = 0; i <= 150; i++){ // Чтобы оно 
     console.log("Hello World!")
 }
+
+// Второй квест
+
+
