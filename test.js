51fbd92101701f52813fa280c0a1556710e2f6c5
Bosbobos <konovalov.matvej@mail.ru>
2020-03-03 22:02:36 +0300

    Ыыыы

--- test.js
+++ test.js
@@ -21,17 +21,32 @@
 
 // Второй квест (предпологаемое решение)
 
-function rabotai () {
-var i
-    for(i = 0; i < 150; i++){ 
+// function rabotai () {
+// var i
+//     for(i = 0; i < 150; i++){ 
     
-    console.log("Hello World!");
-    console.log("Piska")
-    }
+//     console.log("Hello World!");
+//     console.log("Piska")
+//     }
+// }
+// rabotai()
+// rabotai()
+
+// Третий квест
+
+var cvs = document.getElementById("canvas");
+var ctx = cvs.getContext("2d");
+
+var birdYellow = new Image();
+var birdRed = new Image();
+var bg = new Image();
+
+birdYellow.src = "img/birdYellow.png";
+birdRed.src = "img/birdRed.png";
+bg.src = "img/bg.png";
+
+function draw() { 
+    ctx.drawImage(bg, 0, 0);
+    ctx.drawImage(birdYellow, 150, 150);
 }
-rabotai()
-rabotai()
-
-
-
-
+draw ()
