41eeeed76a1e2761faf9da5368640e871162d855
Bosbobos <konovalov.matvej@mail.ru>
2020-03-04 16:20:57 +0300

    3 квест

--- test.js
+++ test.js
@@ -34,30 +34,37 @@
 
 // Третий квест
 
-var cvs = document.getElementById("canvas");
-var ctx = cvs.getContext("2d");
-
-var birdYellow = new Image();
-
-birdYellow.src = "img/birdYellow.png";
-
-function draw() { 
-    ctx.drawImage(birdYellow, 75, 150);
-}
-
 // document.addEventListener("keydown", olert);
 
 // function olert () {
 //     alert ("Засчитало")
 // }
 
+var cvs = document.getElementById("canvas");
+var ctx = cvs.getContext("2d");
+
+var birdYellow = new Image();
+var birdRed = new Image();
+
+birdYellow.src = "img/birdYellow.png";
+birdRed.src = "img/birdRed.png";
+
+// function draw() { 
+//     ctx.drawImage(birdYellow, 75, 150);
+// }
+
 document.addEventListener("keydown", changecolor);
 
+var isBirdYellow = true;
+
 function changecolor () {
-    if (birdYellow.src === "img/birdYellow.png") 
-        birdYellow.src = "img/birdRed.png";
-    else 
-        birdYellow.src = "img/birdYellow.png";
+    if (isBirdYellow === true) {
+    ctx.drawImage(birdYellow, 75, 150);
+    isBirdYellow = false;
+    } else {
+    ctx.drawImage(birdRed, 75, 150);
+    isBirdYellow = true;
+    }
 }
 
-onload = draw;
+onload = changecolor;
